a608316d982068f65e1ebf2fc84e6f2a3f4d5600
rsvasanth <rsvasanth@gmail.com>
2021-08-26 16:44:10 +0530

    first commit

--- booking/booking/web_form/booknow/booknow.py
+++ booking/booking/web_form/booknow/booknow.py
@@ -6,9 +6,9 @@ import frappe
 def get_context(context):
     print("*********Hello***********")
 
-    def on_payment_authorized(self, status_changed_to=None):
-        self.paid = 1
-        self.save(ignore_permissions=True)
+    # def on_payment_authorized(self, status_changed_to=None):
+    #     self.paid = 1
+    #     self.save(ignore_permissions=True)
     context.username = frappe.session.user
     context.booking = frappe.get_last_doc("Booking Desk Reservation")
     context.bookingid = context.booking.name
